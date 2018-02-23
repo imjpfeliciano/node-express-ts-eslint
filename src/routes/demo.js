bf7900f260597cbbc016331fbfe2970cdb325089
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-02-23 12:05:59 -0600

    add docs for demo endpoint

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -6,6 +6,20 @@ const { demo } = demoController;
 
 const api = Router();
 
+/**
+ * @swagger
+ * /api/demo:
+ *   get:
+ *     tags: ["Demo"]
+ *     description: Retrieves a JSON object with <b>hello world<b>
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Success
+ *         schema:
+ *           $ref: '#/definitions/demo'
+ */
 api.get('/', demo.hi);
 
 export default api;
