cad7da4292c5c4fe0ca393cac6167f1c45f0b707
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-02-23 12:03:06 -0600

    add swagger config

--- src/index.js
+++ src/index.js
@@ -3,14 +3,36 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morganBody from 'morgan-body';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 import config from './config';
 import routes from './routes';
+import swaggerOptions from './config/swagger';
 
 const app = express();
 app.server = http.createServer(app);
 
 // middleware
 app.use(cors());
+
+// swagger Documentation
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+const swaggerUiHandler = swaggerUi.setup(swaggerSpec);
+const docsJsonPath = '/api-docs.json';
+
+app.get(docsJsonPath, (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/docs', swaggerUi.serve, (req, res, next) => {
+  if (!req.query.url) {
+    res.redirect(`/docs?url=${req.protocol}://${req.headers.host}${docsJsonPath}`);
+  } else {
+    swaggerUiHandler(req, res, next);
+  }
+});
 
 app.use(bodyParser.json({
   limit: config.bodyLimit,
@@ -27,4 +49,5 @@ app.use('/api', routes);
 app.server.listen(config.port);
 
 console.log(`Started on 'http://localhost:${app.server.address().port}'`);
+
 export default app;
